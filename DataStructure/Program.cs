7c1bf81c93cee83ce4d24be7b12431b2918b8c58
Rana Mateen <a.mateen858@hotmail.com>
2019-08-10 15:13:10 +0500

    1st

---
+++ DataStructure/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructure
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> myList = new List<int>();
+            myList.Add(1);
+            myList.Add(2);
+            myList.Add(3);
+            myList.Add(4);
+            myList.Add(5);
+
+            Console.WriteLine("please enter a number to search");
+
+            int search = Convert.ToInt32((Console.ReadLine()));
+            if (myList.Contains(search))
+            {
+                Console.WriteLine("found");
+            }
+            else
+            {
+                Console.WriteLine("Not found");
+            }
+            Console.ReadKey();
+        }
+    }
+}
